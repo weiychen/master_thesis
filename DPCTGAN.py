FIX: Include gradient penalty calculation in disabled_dp check

DPCTGAN.py
@@ -316,20 +316,20 @@ class DPCTGAN(CTGANSynthesizer):
             discriminator.parameters(), lr=self._discriminator_lr,
             betas=(0.5, 0.9), weight_decay=self._discriminator_decay
         )
-        privacy_engine = opacus.PrivacyEngine( 
-            discriminator,
-            batch_size=self._batch_size,
-            sample_rate = self._batch_size / len(self.data),
-            # sample_size=train_data.shape[0],
-            # alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
-            # noise_multiplier=self.sigma,
-            max_grad_norm=self.max_per_sample_grad_norm,
-            target_epsilon=self.epsilon,
-            epochs = epochs
-            # clip_per_layer=True,
-        )
 
         if not self.disabled_dp:
+            privacy_engine = opacus.PrivacyEngine( 
+                discriminator,
+                batch_size=self._batch_size,
+                sample_rate = self._batch_size / len(self.data),
+                # sample_size=train_data.shape[0],
+                # alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
+                # noise_multiplier=self.sigma,
+                max_grad_norm=self.max_per_sample_grad_norm,
+                target_epsilon=self.epsilon,
+                epochs = epochs
+                # clip_per_layer=True,
+            )
             privacy_engine.attach(optimizerD)
 
         # real_label = 1
@@ -376,12 +376,14 @@ class DPCTGAN(CTGANSynthesizer):
                     y_fake = discriminator(fake_cat)
                     y_real = discriminator(real_cat)
 
-                    pen = discriminator.calc_gradient_penalty(
-                        real_cat, fake_cat, self._device, self.pac)
+                    if self.disabled_dp:
+                        pen = discriminator.calc_gradient_penalty(
+                            real_cat, fake_cat, self._device, self.pac)
                     loss_d = -(torch.mean(y_real) - torch.mean(y_fake))
 
                     optimizerD.zero_grad()
-                    pen.backward(retain_graph=True)
+                    if self.disabled_dp:
+                        pen.backward(retain_graph=True)
                     loss_d.backward()
                     optimizerD.step()
 