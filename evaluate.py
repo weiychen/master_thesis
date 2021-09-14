40f42b94b65ba001576eb01e97bba6ff8306cbb5
Weiychen <b03701237@g.ntu.edu.tw>
2021-09-14 16:42:33 +0200

    Add dataset info to evaluate.py result loading

--- evaluate.py
+++ evaluate.py
@@ -3,12 +3,17 @@ import pandas as pd
 
 # Settings of data to load. This is used to determine the
 # file name
-EPOCHS = 10
-DISABLED_DP = True
+EPOCHS = 500
+DISABLED_DP = False
+dataset = (
+    'datasets/ETM_Configuration2.xes',
+    # 'datasets/financial_log.xes',
+)[0]
 
 def load_sampled_data():
-    RESULTS_FILE_PATTERN = os.path.join("results", "sampled_{}-epochs_dp-{}.csv")
-    csv_file = RESULTS_FILE_PATTERN.format(EPOCHS, not DISABLED_DP)
+    dataset_name = os.path.basename(dataset).split(".")[0]
+    RESULTS_FILE_PATTERN = os.path.join("results", "{}_sampled_{}-epochs_dp-{}.csv")
+    csv_file = RESULTS_FILE_PATTERN.format(dataset_name, EPOCHS, not DISABLED_DP)
     
     try:
         return pd.read_csv(csv_file, index_col=0)
