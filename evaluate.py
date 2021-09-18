aa404bc19172dc79e8fb492092382129bce1f75c
Weiychen <b03701237@g.ntu.edu.tw>
2021-09-18 14:41:49 +0200

    In evaluate.py use DataframeCheckpoint for loading

--- evaluate.py
+++ evaluate.py
@@ -1,25 +1,27 @@
 import os
 import pandas as pd
 
+from checkpoint import DataframeCheckpoint
+
 # Settings of data to load. This is used to determine the
 # file name
-EPOCHS = 500
-ENABLED_DP = True
+EPOCHS_CTGAN = 10
+ENABLED_DP = False
 dataset = (
     'datasets/ETM_Configuration2.xes',
     # 'datasets/financial_log.xes',
 )[0]
 
-def load_sampled_data():
-    dataset_name = os.path.basename(dataset).split(".")[0]
-    RESULTS_FILE_PATTERN = os.path.join("results", "{}_sampled_{}-epochs_dp-{}.csv")
-    csv_file = RESULTS_FILE_PATTERN.format(dataset_name, EPOCHS, ENABLED_DP)
-    
-    try:
-        return pd.read_csv(csv_file, index_col=0)
-    except FileNotFoundError:
-        print("File not found:", csv_file)
 
+
+def load_sampled_data():
+    cp = DataframeCheckpoint(
+        os.path.basename(dataset).split(".")[0], EPOCHS_CTGAN, ENABLED_DP)
+    
+    if cp.exists():
+        return cp.load()
+    else:
+        print("File not found:", cp.save_file)
 
 def evaluate():
     sampled = load_sampled_data()
